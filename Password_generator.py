8e712329ef2751767ac825c749bd0c774f850d0a
TheBigLyadh <138968844+TheBigLyadh@users.noreply.github.com>
2023-07-08 13:33:11 +0530

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,92 @@
+import random
+
+
+def listToString(s):
+
+    # initialize an empty string
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+
+    # return string
+    return str1
+
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password = []
+count_symbols = 1
+count_numbers = 1
+while len(password) <= nr_letters - 1:
+    type = random.randint(1, 3)
+    if type == 1:
+        password.append(random.choice(letters))
+    elif type == 2 and count_numbers <= nr_numbers:
+        password.append(random.choice(numbers))
+        count_numbers += 1
+    elif type == 3 and count_symbols <= nr_symbols:
+        password.append(random.choice(symbols))
+        count_symbols += 1
+
+print(listToString(password))
